2ffe4b6bfbfe0e4d8f6054f2a22c60765b168fef
Sara Boyer <sara.jane.boyer@gmail.com>
2020-03-03 16:13:53 -0700

    js not breaking anymore

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // need to make functions in here for add, subtract, multiply, divide, and total
 // need to make click function show buttons on display
+
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator_keys');
+
+keys.addEventListener("click", e => {
+  if (e.target.matches('button')) {
+    alert("you pressed the button");
+  }
+});
+
+document.getElementById('button').addEventListener("click", function() {
+    alert("you pressed the button");
+});
